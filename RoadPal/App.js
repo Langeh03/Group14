220a970cf3464cff1fc199623ddda351f8918ad7
Ebot-Jenna <168964308+Ebot-Jenna@users.noreply.github.com>
2024-06-09 00:46:34 +0100

    Setting up development environment for the private branch

--- RoadPal/App.js
+++ RoadPal/App.js
@@ -1,26 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, SafeAreaView, ImageBackground } from 'react-native';
-
-const {height} = Dimensions.get("window")
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView>
-       <View>
-        <ImageBackground style ={{
-          height: height/2.5,
-        }}
-         resizeMode = "contain"
-         source = {require("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT7oEWVnE6j7lgv1oIlCrRrfm-eknOFcf4D1A&usqp=CAU")}/>
-      <Text></Text>
-     
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
     </View>
-    </SafeAreaView>
-   
   );
 }
 
-const styles = styleSheet.create{{}};
-
-
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
