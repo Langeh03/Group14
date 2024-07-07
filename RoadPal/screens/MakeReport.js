0d417e0f963a8ef09f0b499613f239d27b73836a
ChinepohDivine-f <divinechinepoh@gmail>
2024-07-07 21:25:26 +0100

    change the color of the input background

--- RoadPal/screens/MakeReport.js
+++ RoadPal/screens/MakeReport.js
@@ -359,6 +359,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 20,
+    backgroundColor: '#eee',
   },
   locationButtonsContainer: {
     flexDirection: "row",
